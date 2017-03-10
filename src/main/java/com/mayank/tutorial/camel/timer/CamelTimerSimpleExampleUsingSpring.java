ac5febbbba620d9a63d19dfd8301e189ef59528a
Mayank Garkoti <mayank.garkoti@siemens.com>
2017-03-11 02:05:02 +0530

    First Commit

---
+++ src/main/java/com/mayank/tutorial/camel/timer/CamelTimerSimpleExampleUsingSpring.java
@@ -0,0 +1,22 @@
+package com.mayank.tutorial.camel.timer;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.spring.SpringCamelContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class CamelTimerSimpleExampleUsingSpring {
+	public static void main(String[] args) throws Exception {
+		ApplicationContext appContext = new ClassPathXmlApplicationContext(
+				"applicationContext.xml");
+		CamelContext camelContext = SpringCamelContext.springCamelContext(
+				appContext, false);
+		try {
+			camelContext.start();
+			Thread.sleep(3000);
+		} finally {
+			camelContext.stop();
+		}
+	}
+}
---
+++ src/main/java/com/mayank/tutorial/camel/timer/CamelTimerTimePatternExample.java
@@ -0,0 +1,33 @@
+package com.mayank.tutorial.camel.timer;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+
+public class CamelTimerTimePatternExample {
+	public static void main(String[] args) throws Exception {
+		CamelContext camelContext = new DefaultCamelContext();
+		try {
+			camelContext.addRoutes(new RouteBuilder() {
+				@Override
+				public void configure() throws Exception {
+					Date future = new Date(new Date().getTime() + 1000);
+
+	                SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+	                String time = sdf.format(future);
+
+	                fromF("timer://simpleTimer?time=%s&pattern=dd-MM-yyyy HH:mm:ss", time)
+	                .setBody(simple("Hello from timer at ${header.firedTime}"))
+	                .to("stream:out");
+				}
+			});
+			camelContext.start();
+			Thread.sleep(3000);
+		} finally {
+			camelContext.stop();
+		}
+	}
+}
